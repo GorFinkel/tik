d8447865a0265553f0550b3f064606e63f71ea67
GorFinkel <97066710+GorFinkel@users.noreply.github.com>
2022-05-05 01:42:26 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-
+//json-server --watch db.json
 let dBox=document.querySelectorAll(".d");
-// let server =' http://localhost:3000/posts/'; not activated
-let playerOne = prompt(`Player 1 (X) what is Your name?`,"Mike");
-let playerTwo = prompt ('Player 2 (O) what is Your name?',"Jake");
+let server =' http://localhost:3000/posts/1';
+let playerX
+let playerY
+let playerTxt=document.getElementById('player')
+let playerOne = confirm('Are you Player 1 (X)?');
 let x= "X"
 let o="O"
-let turnOne=`Now is the ${playerOne}'s (X) turn`;
-let turnTwo=`Now is the ${playerTwo}'s (O) turn`;
-let turnMsg = turnOne;
-let winOneMsg=`${playerOne} (X) has won!`
-let winTwoMsg=`${playerTwo} (O) has won!`
+let turnOne=`Now is the Player 1 (X) turn`;
+let turnTwo=`Now is the Player 2 (O) turn`;
+let turnMsg ;
+let winOneMsg=`Player 1 (X) has won!`
+let winTwoMsg=`Player 2 (O) has won!`
 let tieMsg="End of the round... Tie!"
 let winTimesOne =0 ;
 let winTimesTwo = 0;
 let gameTimes=0;
+let s=""
+let ss=""
+let sss=""
+let totalMsg;
 let box_1=document.getElementById('1')
 let box_2=document.getElementById('2')
 let box_3=document.getElementById('3')
@@ -23,7 +29,9 @@ let box_6=document.getElementById('6')
 let box_7=document.getElementById('7')
 let box_8=document.getElementById('8')
 let box_9=document.getElementById('9')
-let obj={1:'',
+let textLog = document.getElementById('winLog')
+let obj={
+    1:'',
     2:'',
     3:'',
     4:'',
@@ -31,116 +39,233 @@ let obj={1:'',
     6:'',
     7:'',
     8:'',
-    9:''}
-
-
+    9:'',
+    turn:turnOne,
+    won:'',
+    games:"0",
+    winOne:"0",
+    winTwo:"0"};
+if (playerOne){
+        alert('You are Player 1(x)');
+        playerX=true;playerTxt.innerText="You are Player1 (X)"
+    }
+else if (playerOne==false){
+        alert('You are Player 2(O)'); playerY=true;playerTxt.innerText="You are Player2 (Y)"
+    }
+function update(){
+    // do whatever you like here
+    getServerData()
+    setTimeout(update, 1000);
+}
+    update()
+function restart (){ //restart button
+      obj={
+    1:'',
+    2:'',
+    3:'',
+    4:'',
+    5:'',
+    6:'',
+    7:'',
+    8:'',
+    9:'',
+    turn:turnOne,
+    won:'',
+    games:"0",
+    winOne:"0",
+    winTwo:"0"}
+    document.getElementById('winLog').innerText=''
+winTimesOne =0 ;
+ winTimesTwo = 0;
+ gameTimes=0;
+ continueBtn=document.getElementById('continueBtn')
+    if ( continueBtn){
+    continueBtn.remove(continueBtn)
+}
+    sendToServer()
+    sendToServer()
+    sendToServer()
+}
+function sendToServer (){
+        fetch(server, {
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            method: 'PUT',
+            body: JSON.stringify(obj)
+          })
+          updateScreen()
+           }
+function getServerData () {
+        fetch(server)
+        .then(response => response.json())
+        .then(data => {
+          object = JSON.stringify(data);
+        obj=JSON.parse(object)
+        console.log(obj)
+        updateScreen()
+        updateScreen()
+        updateScreen()
+        })
+        };
 function updateScreen(){
-document.getElementById('msg').innerText=turnMsg;
- 
-  document.getElementById(Object.keys(obj)[0]).textContent=obj[1]
-  document.getElementById(Object.keys(obj)[1]).textContent=obj[2]
-  document.getElementById(Object.keys(obj)[2]).textContent=obj[3]
-  document.getElementById(Object.keys(obj)[3]).textContent=obj[4]
-  document.getElementById(Object.keys(obj)[4]).textContent=obj[5]
-  document.getElementById(Object.keys(obj)[5]).textContent=obj[6]
-  document.getElementById(Object.keys(obj)[6]).textContent=obj[7]
-  document.getElementById(Object.keys(obj)[7]).textContent=obj[8]
-  document.getElementById(Object.keys(obj)[8]).textContent=obj[9]
+    let wond
+    box_1.innerText=obj[1]
+    box_2.innerText=obj[2]
+    box_3.textContent=obj[3]
+    box_4.textContent=obj[4]
+    box_5.textContent=obj[5]
+    box_6.textContent=obj[6]
+  box_7.textContent=obj[7]
+  box_8.textContent=obj[8]
+  box_9.textContent=obj[9]
+  turnMsg=obj.turn
+  winTimesOne =obj.winOne;
+  winTimesTwo = obj.winTwo
+ gameTimes=obj.games
+wond=obj.won
+  document.getElementById('msg').innerText=turnMsg;
+  if (gameTimes!==1 ){s='s'}
+  if (winTimesOne!==1  ){ss='s'}
+  if (winTimesTwo!==1  ){sss='s'}
+  if (gameTimes==1 ){s=''}
+  if (winTimesOne==1  ){ss=''}
+  if (winTimesTwo==1  ){sss=''}
+    document.getElementById('winLog').innerText=`During a total of ${gameTimes} game${s}: Player 1 has won ${winTimesOne} time${ss} and Player 2 has won ${winTimesTwo} time${sss} `
+  let contBtn=  document.getElementById('continueBtn')
 
+if (gameTimes>0 &&contBtn===null && wond=='yes')
+{winButtons()
+    wonder()
+}
+function wonder (){
+    if ( box_1.innerText=='' &&
+        box_2.innerText=='' &&
+        box_3.textContent=='' &&
+        box_4.textContent=='' &&
+        box_5.textContent=='' &&
+        box_6.textContent=='' &&
+      box_7.textContent=='' &&
+      box_8.textContent=='' &&
+      box_9.textContent=='')
+       {wond='no'
+       contBtn.remove(contBtn)
+    } 
+    }
+    wonder()
 
 }
-updateScreen();
-
 const game = ()=>{
     dBox.forEach((boxEach) => {
     boxEach.addEventListener('click', play);
     })
 }
 game ()
-
-
 function play (item) {
 const box = item.target.textContent
 let fox = item.target.id
-if (
+let msg='yes'
+if (playerX==true &&
     turnMsg==turnOne && 
     box!==x && 
     box!==o ){
-    turnMsg = turnTwo;
+    obj.turn = turnTwo;
     
     obj[fox]='X'
-
-    updateScreen()
-    testWinCondition (x,winOneMsg)
-
-} else if (
+    sendToServer ()
+    sendToServer ()
+    sendToServer ()
+    testWinCondition (x,winOneMsg,msg)
+} else if (playerY==true &&
     turnMsg==turnTwo &&
     box!==x &&
     box!==o )  {
-    turnMsg = turnOne;
+        obj.turn = turnOne
     obj[fox]='O'
-    updateScreen()
-    testWinCondition (o,winTwoMsg)
-
+    sendToServer ()
+    sendToServer ()
+    sendToServer ()
+    testWinCondition (o,winTwoMsg,msg)
 }
 }
-
-function testWinCondition (value,message){
+function testWinCondition (value,message,mes){
     if ( 
         box_1.textContent == value && 
         box_2.textContent == value && 
         box_3.textContent == value  ) {
-            turnMsg = message
-            updateScreen()
+            obj.turn = message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
      }else if (
         box_4.textContent == value && 
         box_5.textContent == value && 
         box_6.textContent == value) {
-            turnMsg = message
-            updateScreen()
+            obj.turn = message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
     }else if (
         box_7.textContent == value && 
         box_8.textContent == value && 
         box_9.textContent == value) {
-            turnMsg = message
-            updateScreen()
+            obj.turn = message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
     }else if (
         box_1.textContent == value && 
         box_5.textContent == value && 
         box_9.textContent == value) {
-            turnMsg = message
-            updateScreen()
+            obj.turn = message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
     }else if (
         box_3.textContent == value && 
         box_5.textContent == value && 
         box_7.textContent == value) {
-            turnMsg = message
-            updateScreen()
+            obj.turn = message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
     }else if (
         box_1.textContent == value && 
         box_4.textContent == value && 
         box_7.textContent == value) {
-            turnMsg = message
-            updateScreen()
+            obj.turn = message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
     } else if (
         box_2.textContent == value && 
         box_5.textContent == value && 
         box_8.textContent == value) {
-            turnMsg = message
-            updateScreen()
+            obj.turn = message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
     }else if (
         box_3.textContent == value && 
         box_6.textContent == value && 
         box_9.textContent == value) {
-            turnMsg =message
-            updateScreen()
+            obj.turn =message
+            obj.won=mes
+            sendToServer()
+            sendToServer()
+            sendToServer()
             winLog()
     }else if ( 
         box_1.textContent && 
@@ -153,30 +278,33 @@ function testWinCondition (value,message){
         box_8.textContent && 
         box_9.textContent
         ) {
-        turnMsg = tieMsg
-            updateScreen()
-            winLog()
+            obj.turn = tieMsg
+            obj.won=mes
+                    sendToServer()
+        sendToServer()
+        sendToServer()
+                winLog()
     }
 }
 function winLog(){
-if (turnMsg == winTwoMsg){
-    winTimesTwo++
-    gameTimes++
-}else if (turnMsg == winOneMsg) { 
-    winTimesOne++
-    gameTimes++
-}else{gameTimes++}
-  
-let textLog = document.getElementById('winLog')
 let s=''
 let ss=''
 let sss=''
-if (gameTimes>1 ){s='s'}
-if (winTimesOne>1 || winTimesOne===0  ){ss='s'}
-if (winTimesTwo>1 || winTimesTwo===0  ){sss='s'}
-
-textLog.innerText=`During a total of ${gameTimes} game${s}: ${playerOne} has won ${winTimesOne} time${ss} and ${playerTwo} has won ${winTimesTwo} time${sss} `
-winButtons()
+if (obj.turn == winTwoMsg){
+    winTimesTwo++
+    gameTimes++
+    obj.games=gameTimes
+    obj.winTwo=winTimesTwo
+}else if (obj.turn == winOneMsg) { 
+    winTimesOne++
+    gameTimes++
+    obj.games=gameTimes
+    obj.winOne=winTimesOne
+}else{gameTimes++
+}
+  sendToServer()
+sendToServer()
+sendToServer()
 }
 function winButtons(){
     let btn=document.createElement('button')
@@ -184,88 +312,35 @@ function winButtons(){
     btn.id='continueBtn'
     document.body.append(btn)
     restartGame ()
-    
-
-
 }
 function restartGame (){
     let continueBtn = document.getElementById('continueBtn')
     continueBtn.addEventListener('click',()=>{
-    turnMsg=turnOne;
-    obj[1]='';
-    obj[2]='';
-    obj[3]='';
-    obj[4]='';
-    obj[5]='';
-    obj[6]='';
-    obj[7]='';
-    obj[8]='';
-    obj[9]='';
-  
-    updateScreen()
-    continueBtn.remove(continueBtn)
+    obj={
+        1:'',
+        2:'',
+        3:'',
+        4:'',
+        5:'',
+        6:'',
+        7:'',
+        8:'',
+        9:'',
+        turn:turnOne,
+        won:'',
+        games:gameTimes,
+        winOne:winTimesOne,
+        winTwo:winTimesTwo}
+            sendToServer()
+        sendToServer()
+        sendToServer()
+        continueBtn.remove(continueBtn)
+        }
+    )
     }
- )
-    }
+ 
+    
 
 
 
-// more complicated version
-// let clickTimes = 1;
-//  if (clickTimes % 2 !== 0 && box!=="O"&& box!=="X" ){
-//     a.target.textContent ="X";
-//     turnMsg = turnTwo;
-//     updateScreen()
-// clickTimes++
-// console.log(clickTimes)
-
-// } else if (clickTimes % 2 == 0 && box!=="X"&& box!=="O" )  {
-//     a.target.textContent ="O";
-//     turnMsg = turnOne;
-//     updateScreen()
-
-
-
-
-
-
-
-  
-//not activeted yet
-// fetch(server, {
-//     method: 'POST',
-//     body: JSON.stringify({
-//       userId: user_id,
-//       title: new_title,
-//       body: new_body
-//     }),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8"
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log('response: ' + JSON.stringify(data));
-//   })
-
-
-
-// // get data from json file and transform it to an object
-
-// let object ='';
-
-
-// document.getElementById('getMessage').onclick= () => {
-// fetch(server)
-// .then(response => response.json())
-// .then(data => {
-//   object = JSON.stringify(data);
-// object=JSON.parse(object)
-// document.getElementById('message').innerHTML = object[0].title+' '+object[5].body
-
-// console.log(object[0].title)
-
-// })
-
-// };
 
